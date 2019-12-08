0328c996dea24ce343a0fa8302d4b8e8f8790a5a
LeonardoCesca <leonardocescaf@gmail.com>
2019-12-08 18:29:50 -0300

    UPDATE header style

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -15,4 +15,21 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30,60,90, .1);
+}
+
+.logo {
+  padding: 10px 0;
+}
+
+.logo img {
+  width: 90px;
+}
+</style>
