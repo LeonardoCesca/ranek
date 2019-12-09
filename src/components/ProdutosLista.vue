138e9122fdaf09d1bf66bce8b0cf75eca42524ff
LeonardoCesca <leonardocescaf@gmail.com>
2019-12-09 19:46:08 -0300

    ADD axios

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -14,6 +14,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
@@ -22,11 +24,9 @@ export default {
   },
   methods: {
     getProdutos() {
-      fetch("http://localhost:3000/produto")
-        .then(response => response.json())
-        .then(response => {
-          this.produtos = response;
-        });
+      axios.get("http://localhost:3000/produto").then(response => {
+        this.produtos = response.data;
+      });
     }
   },
   created() {
