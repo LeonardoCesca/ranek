69f660beddb5534bfff0378c818a04f25c7cead6
leonardocescaflach <leonardocescaf@gmail.com>
2021-04-02 23:48:43 -0300

    add: product view and pass id props

---
+++ src/views/Produto.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Produto {{id}}</p>
+</template>
+
+<script>
+export default {
+  name: "produto",
+  props: ["id"]
+};
+</script>
+
+<style scoped>
+  p {
+    color: #87f;
+    margin-top: 40px;
+    font-size: 2rem;
+    text-align: center;
+  }
+</style>
