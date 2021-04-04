c4ce7577f40797c834738e1ad9d8846b485516b4
leonardocescaflach <leonardocescaf@gmail.com>
2021-04-04 18:13:35 -0300

    add: product styles and structure

--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -1,10 +1,30 @@
 <template>
-  <p>Produto {{id}}</p>
+<section>
+  <div class="produto" v-if="produto">
+    <ul class="fotos" v-if="produto.fotos">
+      <li v-for="(foto, index) in produto.fotos" :key="index">
+        <img :src="foto.src" :alt="foto.titulo">
+      </li>
+    </ul>
+    <div class="info">
+      <h1>{{produto.nome}}</h1>
+      <p class="preco">
+        {{produto.preco}}
+      </p>
+      <p class="descricao">{{produto.descricao}}</p>
+      <button class="btn" v-if="produto.vendido === 'false'">Comprar</button>
+      <button class="btn" disabled v-else>Produto vendido</button>
+    </div>
+  </div>
+  <PaginaCarregando v-else />
+</section>
 </template>
 
 <script>
 import {api} from '@/services/services.js';
+import PaginaCarregando from '../components/PaginaCarregando.vue';
 export default {
+  components: { PaginaCarregando },
   name: "produto",
   props: ["id"],
   data() {
@@ -27,10 +47,28 @@ export default {
 </script>
 
 <style scoped>
-  p {
-    color: #87f;
-    margin-top: 40px;
-    font-size: 2rem;
-    text-align: center;
+  .produto {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 30px;
+    max-width: 900px;
+    padding: 60px 20px;
+    margin: 0 auto;
+  }
+
+  .preco {
+    color: #e80;
+    font-weight: bold;
+    font-size: 1.5rem;
+    margin-bottom: 40px;
+  }
+
+  .descricao {
+    font-size: 1.2rem;
+  }
+
+  .btn {
+    margin-top: 60px;
+    width: 200px;
   }
 </style>
